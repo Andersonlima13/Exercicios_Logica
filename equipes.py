82d905ce304225f52a168cedf2aec838c5187f84
unknown <anderson.lima18pb@gmail.com>
2023-04-23 15:29:40 -0300

    equipes

--- equipes.py
+++ equipes.py
@@ -1,13 +1,28 @@
-flag = 25
+'O programa deve ler duas equipes e declara o vencedor , que deve ser aquele com 2 pontos a mais que a outra equipe'
+'o mesmo ja  atigindo 25 pontos'
 
+
+
+flag = 25
+print("digite a quantia de pontos")
+   
+A = 0
+B = 0
 
 while True:
-	Equipe = input("digite qual equipe recebe os pontos")
-	if Equipe == "A":
-         pontos = int(input("digite a quantidade de pontos"))
-        if pontos == flag:
-		print("equipe a venceu")
-	elif Equipe == "B":
-		equipeB = pontos
-		if pontos == flag:
-			print("equipe b venceu")
+    Equipe = input("qua equipe recebe os pontos ?") 
+    if Equipe == "A":
+       pontos = int(input(": ")) 
+       A = A + pontos
+       print(f' A equie {Equipe} tem {pontos} pontos')
+       if A == 25 and B + 2 <= A:
+        print("a equipe A venceu")
+        break
+    elif Equipe == "B":
+        pontos = int(input(": "))
+        B = B + pontos
+        print(f' A equie {Equipe} tem {pontos} pontos')
+        if B == 25 and A + 2 <= B:
+            print("equipe b ganhou")
+            break
+    
