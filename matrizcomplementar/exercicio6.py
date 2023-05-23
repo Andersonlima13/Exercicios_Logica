a90f6760394591fb23120c2317d257cc9f33774f
unknown <anderson.lima18pb@gmail.com>
2023-05-22 23:37:53 -0300

    exercicio5

---
+++ matrizcomplementar/exercicio6.py
@@ -0,0 +1,11 @@
+''Uma análise dos acidentes de trânsito está sendo realizada em Manhattan,
+New York. Os cruzamentos das ruas 30 a 38 com as avenidas 1a a 10a foram
+estudadas.
+Faça um programa para, a partir da informação acima, processar a matriz de
+acidentes resultante desse estudo.
+Para cada acidente, será informado o local do cruzamento (Avenida x Rua). O
+programa deverá ler um número desconhecido de acidentes (utilize qualquer
+condição de parada a sua escolha).
+Ao final da leitura dos dados, o programa deverá gerar e exibir a matriz de
+acidentes (obs: exiba na matriz os cabeçalhos de linha e de coluna mostrando
+a identificação das ruas e das avenidas)''''
