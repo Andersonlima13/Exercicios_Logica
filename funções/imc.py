0fd35b4d3cbff7a1f44b749a8d899b9fa9257bc2
unknown <anderson.lima18pb@gmail.com>
2023-06-13 23:37:31 -0300

    commit

--- funções/imc.py
+++ funções/imc.py
@@ -2,7 +2,16 @@
 def calc(altura,peso):
     soma = altura * altura
     imc = peso/soma
-    return imc
+    if imc < 18.5:
+        return "abaixo do peso"
+    elif imc < 24.9 and imc >= 18.5:
+        return "peso ideal"
+    elif imc < 29.9 and imc >= 25:
+        return "sobrepeso"
+    elif imc >= 30:
+        return "obesidade"
+    print(imc)
+    
 
 m = calc(1.68,70)
 print(m)
